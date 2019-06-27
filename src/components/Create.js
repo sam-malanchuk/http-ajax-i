5face334e834188f22691b7a2a477de1b5ebd57a
sam-malanchuk <malanchuksam@gmail.com>
2019-06-26 21:04:10 -0400

    completed add functionality to send to item to the server, added input form styling

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,10 +1,11 @@
 import React from "react";
+import axios from "axios";
 
 class Create extends React.Component {
     state = {
         name: '',
         price: '',
-        imageUrl: '',
+        imageUrl: 'https://picsum.photos/500',
         description: '',
         shipping: ''
     }
@@ -17,6 +18,17 @@ class Create extends React.Component {
 
     createItem = (event) => {
         event.preventDefault()
+
+        const { name, price, imageUrl, description, shipping } = this.state
+        const payload = { name, price, imageUrl, description, shipping }
+
+        axios.post("http://localhost:3333/items", payload)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     render() {
@@ -26,12 +38,14 @@ class Create extends React.Component {
         return (
             <form onSubmit={this.createItem}>
                 <h1>Create New Trinket</h1>
-                
+
                 <input type="text" name="name" value={name} onChange={this.handleChange} placeholder="Name" />
                 <input type="number" name="price" value={price} onChange={this.handleChange} placeholder="Price" />
                 <input type="text" name="imageUrl" value={imageUrl} onChange={this.handleChange} placeholder="Image URL" />
                 <input type="text" name="description" value={description} onChange={this.handleChange} placeholder="Description" />
                 <input type="text" name="shipping" value={shipping} onChange={this.handleChange} placeholder="Shipping" />
+
+                <button type="submit">Create</button>
             </form>
         )
     }
